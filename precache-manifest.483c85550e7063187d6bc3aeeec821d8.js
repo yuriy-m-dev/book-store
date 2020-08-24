9dde99b84e730a11e7b76daa65c4ac01b56dc02e
yuriy-m-dev <fath66426@gmail.com>
2020-08-24 16:57:58 +0300

    Updates

---
+++ precache-manifest.483c85550e7063187d6bc3aeeec821d8.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e573d07b8ea4c162ddffa2a4a4cd4452",
+    "url": "/book-store/index.html"
+  },
+  {
+    "revision": "49edb0130adaa089b6cc",
+    "url": "/book-store/static/css/main.7052083f.chunk.css"
+  },
+  {
+    "revision": "bdfcd0c55057c921e7ef",
+    "url": "/book-store/static/js/2.80c4d5f8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/book-store/static/js/2.80c4d5f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49edb0130adaa089b6cc",
+    "url": "/book-store/static/js/main.6505e6bc.chunk.js"
+  },
+  {
+    "revision": "02fb799921b397677b87",
+    "url": "/book-store/static/js/runtime-main.740a2d2a.js"
+  },
+  {
+    "revision": "9c5d62a3adaabe9e3c857822d9e5ad4f",
+    "url": "/book-store/static/media/HelveticaLTStd-Light.9c5d62a3.woff"
+  },
+  {
+    "revision": "d3afafbe2ccea1d941bd7c7e92a6e78d",
+    "url": "/book-store/static/media/HelveticaLTStd-Light.d3afafbe.woff2"
+  },
+  {
+    "revision": "3c142e671436c4c530a223dd43bafb0b",
+    "url": "/book-store/static/media/HelveticaLTStd-Roman.3c142e67.woff"
+  },
+  {
+    "revision": "9fc94c635bd68fd593fabf41358241ea",
+    "url": "/book-store/static/media/HelveticaLTStd-Roman.9fc94c63.woff2"
+  },
+  {
+    "revision": "661ed9d6fcd4204648b0d027beba4c08",
+    "url": "/book-store/static/media/SegoeWPBold.661ed9d6.woff2"
+  },
+  {
+    "revision": "fcaf6c88cfd7edf89e177fc5f7649f77",
+    "url": "/book-store/static/media/SegoeWPBold.fcaf6c88.woff"
+  },
+  {
+    "revision": "33aee0323d6cc6418ab8c43d02736395",
+    "url": "/book-store/static/media/SegoeWPLight.33aee032.woff"
+  },
+  {
+    "revision": "dd86e0c02240804f8106d46609ca44eb",
+    "url": "/book-store/static/media/SegoeWPLight.dd86e0c0.woff2"
+  },
+  {
+    "revision": "1fb859bfccae21c5ebd8d6e35aef2bcf",
+    "url": "/book-store/static/media/SegoeWPSemiLight.1fb859bf.woff"
+  },
+  {
+    "revision": "f3124629a11c5c5e4123c95672ffa90e",
+    "url": "/book-store/static/media/SegoeWPSemiLight.f3124629.woff2"
+  },
+  {
+    "revision": "7da2026c8e75926cbe8e041b1306c75d",
+    "url": "/book-store/static/media/arrow.7da2026c.svg"
+  },
+  {
+    "revision": "0329ba1f1e5629a0f09cf4c7c3be6ff2",
+    "url": "/book-store/static/media/cart.0329ba1f.svg"
+  },
+  {
+    "revision": "074d092e516e54bf115d288997df731c",
+    "url": "/book-store/static/media/segoe-wp.074d092e.woff2"
+  },
+  {
+    "revision": "a9c4670efeeb1740a2df21d554f6349e",
+    "url": "/book-store/static/media/segoe-wp.a9c4670e.woff"
+  },
+  {
+    "revision": "877e5429d349096cd1b39ba82832dbb4",
+    "url": "/book-store/static/media/trash.877e5429.svg"
+  }
+]);
